a881a44b976dae63bea25822c0a0427f2c69bde1
Gleepoj <63968154+Gleepoj@users.noreply.github.com>
2023-03-05 11:56:27 +0100

    ++ Add point when match motif

--- src/game/aleiiioa/systems/logic/DigesterLogicSystem.hx
+++ src/game/aleiiioa/systems/logic/DigesterLogicSystem.hx
@@ -17,6 +17,7 @@ typedef Points = {pts:Int,status:ContainersStatus};
 class DigesterLogicSystem extends echoes.System {
     var ALL_CONTAINERS:View<ContainerSharedComponent,DigesterFSM>;
     var container_list = new Map();
+    var container_full:Bool = false; 
     var point_map = new Map();
     
     public function new(){
@@ -24,7 +25,7 @@ class DigesterLogicSystem extends echoes.System {
         var b:ContainersStatus = {left: [Gilles,John,Gilles], right:[Gilles,John,Gilles]};
         var c:ContainersStatus = {left: [Gilles,Gilles,Gilles], right:[Gilles,Gilles,Gilles]};
         var d:ContainersStatus = {left: [John,John,John], right:[John,John,John]};
-        var e:ContainersStatus = {left: [Empty,Empty,Empty], right:[Empty,Empty,Empty]};
+        var e:ContainersStatus = {left: [John,John,John], right:[Empty,Empty,Empty]};
 
         point_map.set(1,{pts: 100, status : a});
         point_map.set(2,{pts: 100, status : b});
@@ -34,65 +35,18 @@ class DigesterLogicSystem extends echoes.System {
 
     }
 
-    @u function getContainerContent() {
-        var head = ALL_CONTAINERS.entities.head;
-        container_list.clear();
-        var i = 0;
-        while (head != null) {
-            var csc:ContainerSharedComponent = head.value.get(ContainerSharedComponent);
-            var contain:Array<Container_State> = csc.getContainerArray();
-            container_list.set(i,contain);
-            head = head.next;
-            i++;
-        }
-    }
-
-
-    @u function compare(){
-        var left :Array<Container_State> = container_list.get(0);
-        var right:Array<Container_State> = container_list.get(1);
-
-
-        for(k in point_map.keys()){
-            var value:Points = point_map.get(k);
-            var pts = value.pts;
-            var status = value.status;
-            var pleft:Array<Container_State> = status.left;
-            var pright:Array<Container_State> = status.right;
-            var validate:Int = 0;
-            
-            var l0 = left[0];
-            var l1 = left[1];
-            var l2 = left[2];
-
-            var pl0 = pleft[0];
-            var pl1 = pleft[1];
-            var pl2 = pleft[2];
-
-            var r0 = right[0];
-            var r1 = right[1];
-            var r2 = right[2];
-
-            var pr0 = pright[0];
-            var pr1 = pright[1];
-            var pr2 = pright[2];
-            
-            var a = l0 == pl0 ? 1 : 0;
-            var b = l1 == pl1 ? 1 : 0;
-            var c = l2 == pl2 ? 1 : 0;
-            
-            var e = r0 == pr0 ? 1 : 0;
-            var d = r1 == pr1 ? 1 : 0;
-            var f = r2 == pr2 ? 1 : 0;
-
-            var sum = a+b+c+e+d+f;
-
-            if(sum == 6)
-                trace('Motif : $k nb points: $pts'); 
-        }
+    @u function updateGeneral(){
+        getContainerFullness();
+        if(container_full)
+            compare();
+        
     }
 
     @u function updateDoors(door:DoorComponent,dsc:DigesterSharedComponent,cl:CollisionsListener,csc:ContainerSharedComponent,bb:BoundingBox) {
+        
+        if(container_full)
+            csc.clearContainer();
+
         if(cl.onContactDoor){
             dsc.feed = true;
         }
@@ -116,8 +70,10 @@ class DigesterLogicSystem extends echoes.System {
         if(dsc.digesterState != Free && door.isOpen)
             door.closeDoor();
         
-        if(door.isOpen && csc.isFull)
+        if(door.isOpen && csc.isFull){
             door.closeDoor();
+        }
+
     }
 
 
@@ -148,6 +104,7 @@ class DigesterLogicSystem extends echoes.System {
             csc.fillContainer(dsc.bellyState);
             digest.next(Free);
         }
+
         if(digest.currentState == Free)
             dsc.feed = false;
         
@@ -157,4 +114,63 @@ class DigesterLogicSystem extends echoes.System {
         var val = csc.containers.get(cc.id);
         cc.state = val;
     }
+
+    function compare(){
+        getContainerContent();
+        var l:Array<Container_State> = container_list.get(0);
+        var r:Array<Container_State> = container_list.get(1);
+
+
+        for(k in point_map.keys()){
+            var value:Points = point_map.get(k);
+            var pts = value.pts;
+            var status = value.status;
+            var pl:Array<Container_State> = status.left;
+            var pr:Array<Container_State> = status.right;
+            
+            var a = l[0] == pl[0] ? 1 : 0;
+            var b = l[1] == pl[1] ? 1 : 0;
+            var c = l[2] == pl[2] ? 1 : 0;
+            
+            var e = r[0] == pr[0] ? 1 : 0;
+            var d = r[1] == pr[1] ? 1 : 0;
+            var f = r[2] == pr[2] ? 1 : 0;
+
+            var sum = a+b+c+e+d+f;
+
+            if(sum == 6)
+                trace('Motif : $k  + : $pts'); 
+        }
+    }
+
+    function getContainerFullness() {
+        var head = ALL_CONTAINERS.entities.head;
+        container_full = false;
+        var i = 0;
+        while (head != null) {
+            var csc:ContainerSharedComponent = head.value.get(ContainerSharedComponent);
+            var e = csc.isFull ? 1:0;
+            i +=e;
+            head = head.next;
+            
+        }
+
+        if(i == 2)
+            container_full = true;
+    }
+
+    function getContainerContent() {
+        var head = ALL_CONTAINERS.entities.head;
+        container_list.clear();
+        var i = 0;
+        while (head != null) {
+            var csc:ContainerSharedComponent = head.value.get(ContainerSharedComponent);
+            var contain:Array<Container_State> = csc.getContainerArray();
+            container_list.set(i,contain);
+            head = head.next;
+            i++;
+        }
+    }
+
+
 }
