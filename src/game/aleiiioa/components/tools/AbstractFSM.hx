e8899855703e4d80effaee0e9bbec3a9a98e2f31
Gleepoj <63968154+Gleepoj@users.noreply.github.com>
2023-03-09 13:52:34 +0100

    ++ try refacto fsm

---
+++ src/game/aleiiioa/components/tools/AbstractFSM.hx
@@ -0,0 +1,78 @@
+package aleiiioa.components.tools;
+
+abstract class AbstractState {
+    public function new() {
+        
+    }
+}
+
+private typedef Order = {from:AbstractState,to:AbstractState};
+
+abstract class AbstractFSM {
+    
+    var legal:String = "";
+    var state:AbstractState;
+   // var allowed_transitions:Array<>;
+    var registered_transition:Null<Order>;
+    
+    public function new() {
+    
+   }
+
+}
+
+/* var legal:String = "";
+    var state:Digester_State;
+    var allowed_transitions:Array<Transition_Digest>;
+    var registered_transition:Null<Order_Digest>;
+    
+    public var currentState(get,never):Digester_State; inline function get_currentState() return state;    
+    public var cd:Cooldown;
+    
+    public function new(){
+        
+        cd = new Cooldown(Const.FIXED_UPDATE_FPS);
+        state = Free;
+        
+        allowed_transitions = [
+            {from:Free,   to : [Digest]},
+            {from:Digest, to : [Accept,Reject]},
+            {from:Accept, to : [Spit]},
+            {from:Reject, to : [Spit]},
+            {from:Spit,   to : [Free]}
+        ];
+        
+    }
+    
+    public function next(next_state:Digester_State){
+       
+        if(currentState == next_state){
+            return;
+        }
+        
+        for(transition in allowed_transitions){
+            if(transition.from == state){
+                for(to in transition.to){
+                    if(next_state == to){
+                        registered_transition = {from:state,to:next_state};
+                    }
+                }
+            }
+        }
+        switchToRegisteredTransition();
+    }
+
+    function switchToRegisteredTransition() {
+        if(registered_transition != null){
+            state = registered_transition.to;
+            registered_transition = null;            
+        }  
+    }
+    
+    public function debugLabelUpdate(label:DebugLabel){
+            label.v = "";
+            var state = currentState;
+            var trans = registered_transition;
+            var leg   = legal;
+            label.v = 'cur:$state t:$trans leg:$leg';
+    } */
